c7581a6626a7f759bdb3c99455430a6327cfda4f
panini2518 <hirohiro2222@hotmail.com>
2017-12-13 21:51:32 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/hirohide/ishihara/definitionofhuman/Human.java
@@ -0,0 +1,23 @@
+package jp.techacademy.hirohide.ishihara.definitionofhuman;
+
+import android.util.Log;
+
+class Human extends Animal implements thinkable{
+
+    String hobby;
+
+    public Human(String name, int age, String hobby) {
+        this.name = name;
+        this.age = age;
+        this.hobby = hobby;
+    }
+
+    public void say(){
+        Log.d("javatest", "「私の名前は" + this.name + "です。年は" + this.age + "歳です。」");
+    }
+
+    @Override
+    public void think(){
+        Log.d("javatest", "「私は" + this.hobby + "について考える」");
+    }
+}
---
+++ app/src/main/java/jp/techacademy/hirohide/ishihara/definitionofhuman/thinkable.java
@@ -0,0 +1,5 @@
+package jp.techacademy.hirohide.ishihara.definitionofhuman;
+
+interface thinkable {
+    public void think();
+}
